649681570cdd75f9f391ac1de1b8b0781d258f53
Artem <romanenkoartem13@gmail.com>
2025-10-05 22:07:57 +0200

    first step create application

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-"use strict";
+const numberOfFilms = +prompt('Скільки фільмів ви вже подивились?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один з останніх переглянутих фільмів?', ''),
+    b = prompt('Яку оцінку йому дасте?', ''),
+    c = prompt('Один з останніх переглянутих фільмів?', ''),
+    d = prompt('Яку оцінку йому дасте?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
